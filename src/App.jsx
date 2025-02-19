6c00a246eb0b2eba1fcc477eb1d3668f07f25958
FM_Zuloaga <famelzuloaga@gmail.com>
2025-02-19 00:16:37 -0500

    LA store middle

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Guitar from "./components/Guitar";
+import { db } from "./db";
+
+function App() {
+  //state
+
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExists >= 0) { //existe el carrito
+      const updatedCart = [...cart]
+      updatedCart[itemExists].quantity++
+      setCart(updatedCart)
+    } else {
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+  }
+
+  return (
+    <>
+      <Header
+      cart={cart}
+      />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => {
+            return (
+              <Guitar
+                key={guitar.id} //prop especial al iterar una lista
+                guitar={guitar}
+                cart={cart}
+                setCart={setCart}
+                addToCart={addToCart}
+              />
+            );
+          })}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
